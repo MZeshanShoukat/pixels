8f5e8e4ba682b6758794670902c0014afb708f9c
MZeshanShoukat <89032974+MZeshanShoukat@users.noreply.github.com>
2022-11-07 00:35:44 +0500

    Create ff.js

---
+++ ff.js
@@ -0,0 +1,12 @@
+// dummy-area
+// real-area
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    //Do work
+        
+    var y = document.getElementById('real-area').innerHTML;
+    document.getElementById('real-area').remove();
+    // document.getElementById('creativitylogic').innerHTML = y;
+    console.log('helo this is my')
+});
+console.log('and that also me')
