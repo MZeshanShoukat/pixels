50d789e6a08cc7aaf0128a7fa94c314e9ff5c3ee
MZeshanShoukat <89032974+MZeshanShoukat@users.noreply.github.com>
2022-11-07 00:00:14 +0500

    Create file.js

---
+++ file.js
@@ -0,0 +1,12 @@
+<script>
+// dummy-area
+// real-area
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    //Do work
+        
+    var y = document.getElementById('real-area').innerHTML;
+    document.getElementById('real-area').remove();
+    // document.getElementById('creativitylogic').innerHTML = y;
+});
+</script>
